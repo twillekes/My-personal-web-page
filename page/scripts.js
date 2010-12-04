3d33b995572ac686e2163323ee16d796980904d4
Tom Willekes <tom@twillekesmacbookpro.(none)>
2010-12-03 17:05:52 -0700

    Set up the two layout modes.

--- page/scripts.js
+++ page/scripts.js
@@ -1,4 +1,63 @@
 
+function toWelcomeView()
+{
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML =
+     "\
+            <!--\
+             Front page mode\
+              -->\
+            <div id=\"titlearea\">\
+                <h1 style=\"text-align: center;\">Magpie</h1>\
+                <h2 style=\"text-align: center;\">Landscape and Nature Photography</h2>\
+            </div>\
+\
+            <div id=\"welcome\">\
+                <div class=\"centeredImage\">\
+                     <br/>\
+                    <img src=\"SampleImage.jpg\"/>\
+                    <div id=\"welcomeimagedisplaydiv\"></div>\
+                    <p style=\"text-align: center;\">This is where the welcome text will be</p>\
+                </div>\
+            </div>";
+     
+    var theElement = document.getElementById("contentplaceholder");
+    theElement.appendChild(newdiv);
+}
+
+function toImageView()
+{
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML =
+     "\
+            <!--\
+             Image display mode\
+              -->\
+            <div id=\"thumbbar\">\
+                <p  style=\"text-align: center;\">Image Category Name</p>\
+                <div class=\"centeredImage\">\
+                    <img src=\"SampleImage.jpg\" class=\"thumbnailImage\"/>\
+                \
+                    <div id=\"thumbdisplaydiv\"></div>\
+                </div>\
+            </div>\
+            \
+            <div id=\"copyrightfooter\">\
+                <h3 style=\"text-align: center;\">Copyright 2003-2010 Tom Willekes</h3>\
+            </div>\
+\
+            <div id=\"imagedisplayarea\">\
+                <div class=\"centeredImage\">\
+                    <h3 style=\"text-align: center;\">Image Title Here</h3>\
+                    <br/>\
+                    <img src=\"SampleImage.jpg\"/>\
+                    <div id=\"imagedisplaydiv\"></div>\
+                </div>\
+            </div>";
+     
+    var theElement = document.getElementById("contentplaceholder");
+    theElement.appendChild(newdiv);
+}
 
 function loadImages()
 {
